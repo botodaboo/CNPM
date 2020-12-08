d95c06071657a97b268e020eabd075bc71e9cdb8
recca5p <73675537+recca5p@users.noreply.github.com>
2020-12-08 17:12:09 +0700

    Update MenuManage.sql

--- MenuManage.sql
+++ MenuManage.sql
@@ -7,38 +7,38 @@ create table Food
     Food_ID varchar(10),
     Food_name nvarchar(40),
     Price int,
-    Description nvarchar(255),
+    [Description] nvarchar(255) NULL,
     primary key (Food_ID)
 )
 --------------------------------------------------------------------------------
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('001', N'Zensai moriawase', 450000, "")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('002', N'Shiori Ginnan', 95000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('003', N'Yaki Gyoza', 1100000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('004', N'Nasu Dengaku', 75000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('005', N'Jidori Karaage', 150000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('006', N'Wagyu Korokke', 85000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('007', N'Taka Yaki Ball', 95000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('008', N'Ebi Harumaki', 150000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('009', N'Kani Nanban Zuke', 225000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('010', N'Hiyashi Wakame', 105000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('011', N'Ebi Mayo', 165000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('012', N'Torikawa Senbei', 75000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('013', N'Ebi Tempura', 280000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('014', N'Tempura Mori', 265000,"")
-insert into Food (Food_ID, Food_name, Price, Description)
-values ('015', N'Salmon Truffle Cream', 278000,"")
+insert into Food
+values ('001', N'Zensai moriawase', 450000, NULL)
+insert into Food
+values ('002', N'Shiori Ginnan', 95000,NULL)
+insert into Food
+values ('003', N'Yaki Gyoza', 1100000,NULL)
+insert into Food
+values ('004', N'Nasu Dengaku', 75000,NULL)
+insert into Food
+values ('005', N'Jidori Karaage', 150000,NULL)
+insert into Food
+values ('006', N'Wagyu Korokke', 85000,NULL)
+insert into Food
+values ('007', N'Taka Yaki Ball', 95000,NULL)
+insert into Food
+values ('008', N'Ebi Harumaki', 150000,NULL)
+insert into Food
+values ('009', N'Kani Nanban Zuke', 225000,NULL)
+insert into Food
+values ('010', N'Hiyashi Wakame', 105000,NULL)
+insert into Food
+values ('011', N'Ebi Mayo', 165000,NULL)
+insert into Food
+values ('012', N'Torikawa Senbei', 75000,NULL)
+insert into Food
+values ('013', N'Ebi Tempura', 280000,NULL)
+insert into Food
+values ('014', N'Tempura Mori', 265000,NULL)
+insert into Food
+values ('015', N'Salmon Truffle Cream', 278000,NULL)
 ---------------------------------------------------------------------------
